910c5caca67b7041c0de29a59fa99f30266f34db
Adithyan KV <adithyankv007@gmail.com>
2020-07-11 23:12:22 +0530

    Styling sign up form
    Trying to style the sign up form with custom styling.
    Adding custom flash messages for validation errors.
    Having styling issues

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     path('signup', views.signup, name='signup_page'),
-    path('messages', views.info_messages, name='messages_page')
 ]
--- users/views.py
+++ users/views.py
@@ -8,12 +8,12 @@ def signup(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data.get('username')
-            messages.success(request, f'{username.capitalize()}')
-            return redirect('messages_page')
+            messages.success(
+                request, f'Hi {username}, your account has been created. Go to the Login page to access your account')
+            return redirect('home_page')
+        else:
+            error_list = form.error_messages
+            messages.error(request, f'Sorry {error_list} error happened')
     else:
         form = UserCreationForm()
     return render(request, 'users/signup_page.html', {'form': form})
-
-
-def info_messages(request):
-    return render(request, 'users/message_page.html')
