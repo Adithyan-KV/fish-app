27dc25514b80056d24d6c0f3f7645375cebb306e
Adithyan KV <adithyankv007@gmail.com>
2020-07-12 02:41:08 +0530

    Added custom sign up form
    -A custom up form object was added to the Users app.
    -Changed the css of the container box of sign up page
     so that it resizes to account for error messages.
    -Still have to implement constant aspect ratio for said box
    -Still have to make sign up page responsive

--- users/views.py
+++ users/views.py
@@ -1,20 +1,23 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import SignUpForm
 
 
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data.get('username')
             messages.success(
-                request, f'Hi {username}, your account has been created. Go to the Login page to access your account')
+                request, f'Hi {username.capitalize()}!,'
+                ' your account has been created.'
+                ' Go to the Login page to access your account')
             return redirect('home_page')
         # else:
         #     error_list = form.error_messages
         #     for error in error_list:
         #         messages.error(request, f'Sorry {error_list} error happened')
     else:
-        form = UserCreationForm()
+        form = SignUpForm()
     return render(request, 'users/signup_page.html', {'form': form})
