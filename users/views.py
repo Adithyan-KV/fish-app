9fcd36a0384df11a9162fb4de1e391848460c1c5
Adithyan KV <adithyankv007@gmail.com>
2020-07-13 17:04:21 +0530

    Refactored CSS and added User registration to database -The CSS was refactored so that all the redundant code was changed to  a base.css file (navbar, messages etc.) which every HTML file inherits  along with the CSS file with stylings specific to that page. -The User Registration to database was added. Now upon filling the form  and submitting, a New user is actually added to the database. -TODO: Still have to expand the user registration to add email  verification.

--- users/views.py
+++ users/views.py
@@ -8,6 +8,7 @@ def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
             messages.success(
                 request, f'Hi {username.capitalize()}!,'
